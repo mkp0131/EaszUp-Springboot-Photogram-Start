c73a03091391b3c7dea2b7dc3011bc5719e6b798
mkp0131 <mkp0131@naver.com>
2023-08-31 12:55:22 +0900

    로그인 연결

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -1,5 +1,8 @@
 package com.cos.photogramstart.config.auth;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,11 +11,27 @@ import org.springframework.stereotype.Service;
 /**
  * 스프링 시큐리티에서 로그인시 UserDetailService 가 실행
  */
+@RequiredArgsConstructor
 @Service
 public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * 패스워드는 알아서 체크
+     * 리턴이 잘되면 세션을 생성
+     */
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        System.out.println(s);
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+
+        if(userEntity == null) {
+            return null;
+        }
+        else {
+            return new PrincipalDetails(userEntity);
+        }
+
+
     }
 }
--- src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
+++ src/main/java/com/cos/photogramstart/domain/user/UserRepository.java
@@ -2,4 +2,9 @@ package com.cos.photogramstart.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer> {
+    // JPA Naming Query
+    User findByUsername(String username);
+
+
+}
