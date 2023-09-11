41ed9e4adacd5db080e44d8550b783c93d0e5560
mkp0131 <mkp0131@naver.com>
2023-09-11 22:36:42 +0900

    갯수 쿼리 추가

--- src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
+++ src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
@@ -9,16 +9,16 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
 
     @Modifying // INSERT, DELETE, UPDATE 를 네이티브 쿼리로 작성하려면 해당 어노테이션 필요!!
     // 성공시 변경된 행의 갯수가 리턴됨. -1 은 에러
-    @Query(value = "INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
+    @Query(value = "INSERT INTO Subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
     int mSubscribe(@Param("fromUserId") int fromUserId, @Param("toUserId") int toUserId);
 
     @Modifying
-    @Query(value = "DELETE FROM subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId", nativeQuery = true)
+    @Query(value = "DELETE FROM Subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId", nativeQuery = true)
     int mUnSubscribe(@Param("fromUserId") int fromUserId, @Param("toUserId") int toUserId);
 
-    @Query(value = "SELECT COUNT(*) FROM subscribe WHERE fromUserId = :principalId AND toUserId = :pageUserId", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM Subscribe WHERE fromUserId = :principalId AND toUserId = :pageUserId", nativeQuery = true)
     int mSubscribeState(@Param("principalId") int principalId, @Param("pageUserId") int pageUserId);
 
-    @Query(value = "SELECT COUNT(*) FROM subscribe WHERE fromUserId = :pageUserId", nativeQuery = true)
+    @Query(value = "SELECT COUNT(*) FROM Subscribe WHERE fromUserId = :pageUserId", nativeQuery = true)
     int mSubscribeCount(@Param("pageUserId") int pageUserId);
 }
